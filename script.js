aa01704118d5ec53e62bd4e863f4ba254fdcf8e0
Absar Ahmad <121477562+Absar11@users.noreply.github.com>
2025-06-03 10:01:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", () => {
+	const h1 = document.createElement("h1");
+	h1.textContent = "Entered Metaverse";
+	document.getElementById("status").replaceWith(h1);
+})
